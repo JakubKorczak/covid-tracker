7a0a5dbc4d0404702d58558de4e381d350af232b
Jakub <jsz.korczak@gmail.com>
2021-05-04 18:50:47 +0200

    Update HamburgerButton.js

--- src/components/molecules/HamburgerButton/HamburgerButton.js
+++ src/components/molecules/HamburgerButton/HamburgerButton.js
@@ -4,7 +4,8 @@ import { ReactComponent as CloseIcon } from 'assets/icons/close_icon.svg';
 import StyledHamburgerButton from './StyledHamburgerButton';
 
 const HamburgerButton = ({ isNavOpen, toggleNavVisibility }) => {
-  const className = isNavOpen ? 'close-button' : '';
+  const className = isNavOpen ? 'close-button' : ''; //think about using classNames lib https://www.npmjs.com/package/classnames
+  const className = isNavOpen || 'close-button'; //check this - maybe null or sth 
   return (
     <StyledHamburgerButton className={className} onClick={() => toggleNavVisibility()}>
       {isNavOpen ? <CloseIcon /> : <MenuIcon />}
