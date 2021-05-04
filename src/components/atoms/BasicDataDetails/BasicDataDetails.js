545088aea54bd5a233bef2c76b0a70aff22955ff
Jakub <jsz.korczak@gmail.com>
2021-05-04 18:41:13 +0200

    Update BasicDataDetails.js

--- src/components/atoms/BasicDataDetails/BasicDataDetails.js
+++ src/components/atoms/BasicDataDetails/BasicDataDetails.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import StyledBasicDataDetails from './StyledBasicDataDetails';
 
-const BasicDataDetails = ({ type, content, children }) => {
+const BasicDataDetails = ({ type, content, children }) => { //pass props instead of using composition - children
   return (
     <StyledBasicDataDetails className={type}>
       {children} <span className={type}>{content}</span>
