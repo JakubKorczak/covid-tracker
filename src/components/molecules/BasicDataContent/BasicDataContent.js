44b31c93f7a8adf6d10a012b9132eebf7d2c7e97
Jakub <jsz.korczak@gmail.com>
2021-05-04 18:43:44 +0200

    Update BasicDataContent.js

--- src/components/molecules/BasicDataContent/BasicDataContent.js
+++ src/components/molecules/BasicDataContent/BasicDataContent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import StyledBasicDataContent from './StyledBasicDataContent';
 import BasicDataDetails from 'components/atoms/BasicDataDetails/BasicDataDetails';
 
-const BasicDataContent = ({ oneMilion, sum, today, type }) => {
+const BasicDataContent = ({ oneMilion, sum, today, type }) => { //pass props instead of using composition - children
   return (
     <StyledBasicDataContent>
       <BasicDataDetails type={type} content={sum}>
